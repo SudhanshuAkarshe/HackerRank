f31949f1b54df7fca30db0649ddc706959cc3a99
SudhanshuAkarshe <42716153+SudhanshuAkarshe@users.noreply.github.com>
2025-04-14 18:10:40 +0100

    Add files via upload

---
+++ Drawing Book/code.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the pageCount function below.
+     */
+    static int pageCount(int n, int p) {
+
+    int temp1;
+    int temp2;
+    temp1=p/2; 
+    temp2=(n-p)/2;
+
+    if(n%2==0 && p%2==1)
+        temp2=(n-p+1)/2;
+
+    if(temp1>=temp2)
+    {
+        return temp2;
+    }
+    else
+    {
+        return temp1;
+    }
+   }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int p = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int result = pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
