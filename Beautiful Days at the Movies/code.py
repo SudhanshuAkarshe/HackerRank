45ad89e26b13b3022f5637d89ee3b0f23534e12d
SudhanshuAkarshe <42716153+SudhanshuAkarshe@users.noreply.github.com>
2025-05-27 13:02:13 +0100

    Add files via upload

---
+++ Beautiful Days at the Movies/code.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the beautifulDays function below.
+def beautifulDays(i, j, k):
+    count = 0
+    for ele in range(i,j+1):
+        temp = (ele-int(str(ele)[::-1]))/ k
+        if temp.is_integer():
+            count+=1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ijk = input().split()
+
+    i = int(ijk[0])
+
+    j = int(ijk[1])
+
+    k = int(ijk[2])
+
+    result = beautifulDays(i, j, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
